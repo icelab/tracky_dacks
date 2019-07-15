b4fe3121829fb5adf50b08a711ac2497a95b0c11
Andrew Croome <andrewcroome@gmail.com>
2019-07-15 15:11:36 +1000

    Add support for skipping tracking per request.

--- lib/tracky_dacks/plugin.rb
+++ lib/tracky_dacks/plugin.rb
@@ -18,12 +18,13 @@ module TrackyDacks
       app.plugin :sinatra_helpers
     end
 
-    def self.configure(app, runner: Job.method(:perform_async), handlers: DEFAULT_HANDLERS, handler_options: {})
+    def self.configure(app, runner: Job.method(:perform_async), handlers: DEFAULT_HANDLERS, handler_options: {}, skip_tracking_if: nil)
       plugin_opts = app.opts[:tracky_dacks] = {}
 
       plugin_opts[:runner] = runner
       plugin_opts[:handlers] = handlers
       plugin_opts[:handler_options] = handler_options
+      plugin_opts[:skip_tracking_if] = skip_tracking_if
     end
 
     module RequestMethods
@@ -33,10 +34,17 @@ module TrackyDacks
             on /#{key}\.?(\w+)?/ do |format|
               handler = handler_class.new(roda_class.opts[:tracky_dacks][:handler_options].merge(handler_options))
 
+              skip_tracking =
+                if roda_class.opts[:tracky_dacks][:skip_tracking_if]
+                  roda_class.opts[:tracky_dacks][:skip_tracking_if].call(self)
+                else
+                  false
+                end
+
               roda_class.opts[:tracky_dacks][:runner].(
                 handler,
                 params.merge("referrer" => referrer)
-              )
+              ) unless skip_tracking
 
               if format == "png"
                 send_file IMAGE_PATH, disposition: "inline"
